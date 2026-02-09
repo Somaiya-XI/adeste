c6876baa0d1c31413eb55ec5bbbdb73ec26121ad
Dana AlGhadeer <danaabdullahalghadeer@gmail.com>
2026-02-09 15:23:59 +0300

    Package

--- levelUp/Models/Habit.swift
+++ levelUp/Models/Habit.swift
@@ -24,15 +24,22 @@ protocol HabitManager  {
     var id: String { get set }
     var name: String { get set }
     
-    func calculateHabitProgress()
+    func calculateHabitProgress() -> HabitProgress
+}
+
+struct HabitProgress {
+    /// 0.0 → 1.0
+    let fraction: Double
+    let isCompleted: Bool
 }
 
 
 struct StepsManager: HabitManager {
     var id: String
     var name: String
-    func calculateHabitProgress() {
+    func calculateHabitProgress() -> HabitProgress {
         //Custom for each habit
+        return HabitProgress(fraction: 0, isCompleted: false)
     }
     
     var stepCount: Int
@@ -40,3 +47,47 @@ struct StepsManager: HabitManager {
     
 
 }
+
+struct WaterManager: HabitManager {
+    var id: String
+    var name: String
+    func calculateHabitProgress() -> HabitProgress {
+        //Custom for each habit
+        return HabitProgress(fraction: 0, isCompleted: false)
+    }
+    var waterCount: Int
+    var isCompleted: Bool
+
+}
+
+enum Prayer: String, CaseIterable, Codable, Hashable {
+    case fjr = "Fajr"
+    case dhr = "Dhuhr"
+    case asr = "Asr"
+    case mgb = "Magrib"
+    case ish = "Isha"
+}
+
+struct PrayerTimesDay {
+    // start of the day in user's timezone
+    let dayStart: Date
+
+    // prayer times
+    let times: [Prayer: Date]
+}
+
+import Foundation
+
+protocol PrayerTimesProviding {
+    func prayerTimes(for dayStart: Date) async throws -> PrayerTimesDay
+}
+
+//struct PrayerManager: HabitManager {
+//    var id: String
+//    var name: Prayer
+//    func calculateHabitProgress() -> HabitProgress {
+//        return HabitProgress(fraction: 0, isCompleted: false)
+//    }
+//}
+
+
