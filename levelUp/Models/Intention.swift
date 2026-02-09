084ffd7076c04cfabbcc872ed347a0b31521b6b1
Dana AlGhadeer <danaabdullahalghadeer@gmail.com>
2026-02-09 19:00:46 +0300

    merge(Dana) to main

--- levelUp/Models/Intention.swift
+++ levelUp/Models/Intention.swift
@@ -10,18 +10,33 @@ import SwiftData
 
 @Model
 class Intention: Identifiable {
-    var id: UUID
+    var id: String
     var title: String
     //SF Symbols to represent the intention
     var icon: String
     var apps: [String]?
-    var focus: Int = 0
-    var distraction: Int = 0
+    var focus: Double = 0.0
+    var distraction: Double = 0.0
     
-    public init(id: UUID = UUID(), title: String, icon: String = "", focus: Int = 0, distraction: Int = 0) {
-        self.id = id
+    public init(title: String, icon: String = "plus", focus: Double = 0.0, distraction: Double = 0.0) {
+        self.id = UUID().uuidString
         self.title = title
         self.icon = icon
+        self.focus = focus
+        self.distraction = distraction
         self.apps = nil
+    }
+    
+    
+    func registerFocus() {
+        self.focus += 1.0
+    }
+    
+    func registerDistraction() {
+        self.distraction += 1.0
+    }
+
+    func editApps(for intention: inout Intention, with apps: [String]) {
+        
     }
 }
