2291b775b5b9ab37afc437d71011e28c31dd03f4
Mayar <Mayar>
2026-02-09 18:00:06 +0300

    Add IntentionsView

---
+++ levelUp/Views/IntentionsListView.swift
@@ -0,0 +1,17 @@
+//
+//  IntentionsListView.swift
+//  levelUp
+//
+
+import SwiftUI
+
+struct IntentionsListView: View {
+   
+    var body: some View {
+        Text("List of Intentions (Coming Soon ^^)")
+    }
+}
+
+#Preview {
+    IntentionsListView()
+}
