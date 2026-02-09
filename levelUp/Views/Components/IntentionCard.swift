44b6e6201aa8b057a8bf90ad6cf1ed2afb5f65f6
Somaiya <4111674@upm.edu.sa>
2026-02-09 13:45:25 +0300

    feat(intentions): create intentions base logic and functionalities

---
+++ levelUp/Views/Components/IntentionCard.swift
@@ -0,0 +1,102 @@
+//
+//  IntentionCard.swift
+//  levelUp
+//
+//  Created by Somaiya on 21/08/1447 AH.
+//
+
+import SwiftUI
+
+struct IntentionCard: View {
+    @State var name = ""
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            let width = size.width
+            let height = size.height
+            
+            ZStack{
+                Image("Intentions1")
+                    .ignoresSafeArea()
+                    .overlay {
+                        Color.black.opacity(0.6)
+                            .ignoresSafeArea()
+                    }.blur(radius: 4)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 16)
+                        .glassEffect(.regular, in: .rect(cornerRadius: 16))
+                        .frame(width: (width * 0.9), height: (height * 0.58))
+                    
+                    VStack (alignment: .leading) {
+                        HStack(alignment: .center){
+                            Text("New Intention")
+                                .font(.title2.bold())
+                            Spacer()
+                            Button{
+                                
+                            }label:{
+                                Image(systemName: "xmark.circle")
+                                    .font(.title)
+                            }.tint(.brand)
+                        }
+                        
+                        HStack{
+                                Circle()
+                                .fill(.secColorBlue.opacity(0.7))
+                                    .frame(width: width * 0.16)
+                                    .overlay {
+                                        Image(systemName: "pencil")
+                                            .font(.largeTitle)
+                                    }
+                                                           
+                                TextField("e.g, Reading", text: $name)
+                                    .padding(16)
+                                    .background(.gray.opacity(0.4))
+                                    .clipShape(.capsule)
+                            }
+                        Text("Apps").font(.title2)
+                            ScrollView(.vertical){
+                                LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))], alignment: .center, spacing: 8){
+                                    ForEach(1...14, id:\.self ){ i in
+                                        RoundedRectangle(cornerRadius: 16)
+                                            .fill(.black.opacity(0.3))
+                                            .frame(width: width * 0.16, height: width * 0.16).padding(.horizontal, 8)
+                                    }
+                                    
+                                }.padding(.vertical, 10)
+                                    .padding(.horizontal, 4)
+                            }.scenePadding(.vertical)
+                            .safeAreaPadding(.horizontal)
+                            .frame(maxHeight: width * (0.16*2.9))
+                            .background(.gray)
+                            .clipShape(.rect(cornerRadius: 20))
+
+                            
+                        Button{}
+                        label:{
+                            Text("Done")
+                                .font(.title3.bold())
+                                .padding(8)
+                        }
+                            .buttonSizing(.flexible)
+                            .buttonStyle(.bordered)
+                            .tint(.secColorBerry)
+
+                       Spacer()
+                      
+                    }.padding(.top, height * 0.2)
+                    .padding(16)
+                    
+                }.padding()
+                
+            }
+        }
+
+        }
+
+}
+
+#Preview {
+    IntentionCard()
+}
